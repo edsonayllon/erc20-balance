5b42e5cbd88ee009c71e505aed859d3f36438250
Edson Ayllon <eayllon1@gmail.com>
2020-11-13 21:12:45 -0500

    Get balance

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,54 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { sUSDaddr } from './consts';
+import { minErc20 } from './abi';
+import Web3 from 'web3';
 
 function App() {
+  //let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+  //const contract = web3.eth.contract(minErc20).at(sUSDaddr);
+  const [account, setAccount] = useState('');
+  const [balance, setBallance] = useState('');
+  const [decimals, setDecimals] = useState('');
+  const web3 = new Web3(Web3.givenProvider);
+  
+  
+  const handleConnect = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(accounts[0]);
+  }
+
+  const getBalance = async () => {
+    let contract = new web3.eth.Contract(minErc20, sUSDaddr);
+    contract.methods.balanceOf(account).call().then(balance => {
+      setBallance(balance);
+    })
+    contract.methods.decimals().call().then(decimals => {
+      setDecimals(decimals)
+    })
+  }
+
+  useEffect(() => {
+    if (account !== '') {
+      getBalance();
+    }
+  }, [account])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Address: {account}</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          balance: {balance / (10**decimals)}
         </p>
-        <a
+        <button
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick = {handleConnect}
         >
-          Learn React
-        </a>
+          Connect Wallet
+        </button>
       </header>
     </div>
   );
---
+++ client/src/consts.js
@@ -0,0 +1,5 @@
+const sUSDaddr = "0x57ab1e02fee23774580c119740129eac7081e9d3"
+
+export {
+    sUSDaddr
+}
